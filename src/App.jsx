34e136b6787ec92126b2aa481d75f5f77f5c644b
Tharun <tharungowda30115@gmail.com>
2025-03-04 22:35:30 +0530

    Quick commit (No specific file changes detected)

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
+import Footer from "./components/footer";
 
 const App = () => {
   return(
     <> 
       <div className="bg-slate-900 fixed w-full z-10">
         <Navbar />
+        <Footer />
       </div>
 
       <Routes>
---
+++ src/components/footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        <footer className='bg-slate-900 justify-center'>
+            Made with 💖 by tharun
+        </footer>
+    </div>
+  )
+}
+
+export default Footer
